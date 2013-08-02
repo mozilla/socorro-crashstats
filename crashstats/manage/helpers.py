9c4fa05150610e670071d50af324a4f9e0956b01
lonnen <chris.lonnen@gmail.com>
2013-08-02 14:02:07 -0700

    bug 901091 - shut it down

--- crashstats/manage/helpers.py
+++
@@ -1,8 +0,0 @@
-from jingo import register
-
-
-@register.filter
-def show_percentage(value):
-    if value == int(value):  # a whole number like 10.0
-        return '%d%%' % value
-    return "%.1f%%" % value
